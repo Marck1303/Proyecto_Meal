74c99e5c1b4f648b7b585ebbf3fb623c49971c6e
AlanZalazar <alan_zz_@hotmail.com>
2025-07-07 18:25:30 -0300

    agregue views/Details.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Details from "./views/Details";
 
 function App() {
-
   return (
     <>
-  
+      <Details></Details>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
