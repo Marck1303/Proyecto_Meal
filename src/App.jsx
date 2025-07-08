ac3ab297f267a4d99c7cd9343e15a3305d795112
Steven <anthonystevenjulian@gmail.com>
2025-07-08 19:57:06 -0400

    agrege_categoryCardsCotiner_y_pruebas_en_CategoryMeals

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home.jsx";
-import Detail from "./pages/Detail.jsx";
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home.jsx';
+import Detail from './pages/Detail.jsx';
+import CategoryMeals from './pages/CategoryMeals.jsx';
 
 function App() {
   return (
@@ -8,9 +10,11 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/detail/:id" element={<Detail />} />
+        <Route path="/category/:categoryName" element={<CategoryMeals />} />
       </Routes>
     </BrowserRouter>
   );
 }
 
 export default App;
+
