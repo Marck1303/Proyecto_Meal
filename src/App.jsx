50063c425251d52a0586faf2cce7b2f0ba7dd19f
Luis Zegarra <luchozegarra@gmail.com>
2025-07-08 21:15:17 -0500

    avances funcionales a Detail.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
 
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home.jsx';
 import Detail from './pages/Detail.jsx';
 import CategoryMeals from './pages/CategoryMeals.jsx';
 
 function App() {
   return (
-    <BrowserRouter>
+    <>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/detail/:id" element={<Detail />} />
         <Route path="/category/:categoryName" element={<CategoryMeals />} />
       </Routes>
-    </BrowserRouter>
+    </>
   );
 }
 
