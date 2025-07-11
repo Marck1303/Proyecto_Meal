5a2c7d67170559d1cf14b3a3a4b70f09bc770dae
prueba1 <marcoariasmedrano@hotmail.com>
2025-07-11 09:32:07 -0500

    Reseña-opacidad

--- src/pages/Reseñas.jsx
+++ src/pages/Reseñas.jsx
@@ -20,7 +20,7 @@ const Estrellas = ({ cantidad }) => (
 // Modal genérico
 function Modal({ children, onClose }) {
   return (
-    <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
+    <div className="fixed inset-0 bg-black/50 bg-opacity-50 flex items-center justify-center z-50">
       <div className="relative bg-white rounded shadow-lg max-w-lg w-full mx-4">
         <button
           onClick={onClose}
